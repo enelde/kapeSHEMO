eb2ae2ecb7eb01920b698451c19730906df6b0e8
enelde <sushierz.sashimierz@gmail.com>
2014-07-19 13:41:25 +0700

    DB kurang gambar

---
+++ wisata.sql
@@ -0,0 +1,119 @@
+1	0	1	Balai Pemuda	Jalan Gubernur Suryo	NULL	NULL	-7.264054	112.745378
+2	0	1	Gedung balaikota surabaya	Jalan Walikota Mustajab	NULL	NULL	-7.260052	112.746805
+3	0	1	Gedung Bank Mandiri	Jalan Pahlawan	NULL	NULL	-7.243598	112.738649
+4	0	1	Gedung Cerutu	Jalan Rajawali	NULL	NULL	-7.236627	112.737385
+5	0	1	Gedung Grahadi	Jalan Walikota Mustajab	NULL	NULL	-7.263399	112.743254
+6	0	1	Gedung Kantor POS besar	Jalan Indrapura	NULL	NULL	-7.243023	112.737315
+7	0	1	Gedung Polrestabes	Jalan Niaga	NULL	NULL	-7.238809	112.737116
+8	0	1	Gedung Rumah Sakit Darmo	Jalan Darmo	NULL	NULL	-7.287361	112.738951
+9	0	1	Gedung St Louise	Jalan Polisi Istimewa	NULL	NULL	-7.28031	112.743184
+10	0	1	Gedung Wismilak	Jalan Darmo	NULL	NULL	-7.281805	112.740132
+11	0	1	Hotel IBIS	Jalan Rajawali	NULL	NULL	-7.236894	112.737309
+12	0	1	Hotel majapahit	Jalan Tunjungan	NULL	NULL	-7.26019	112.739579
+13	0	1	Kantor Gubernur	Jalan Pahlawan	NULL	NULL	-7.246269	112.739187
+14	0	1	Kantor POS Pembantu Simpang	Jalan Gubernur Suryo	NULL	NULL	-7.263697	112.743361
+15	0	1	Kantor Soeara Asia (Gedung Berantas)	Jalan Pahlawan	NULL	NULL	-7.244513	112.738544
+16	0	1	Kawasan Jembatan Merah	Jalan Rajawali	NULL	NULL	-7.236669	112.738249
+17	0	1	Kawasan Tunjungan	Jalan Tunjungan	NULL	NULL	-7.259584	112.7391
+18	0	1	Kawasan Wonokromo dan Pintu Air Jagir	Jalan Wonokromo	NULL	NULL	-7.300344	112.740494
+19	0	1	Pelabuhan Kalimas	Jalan Kalimas	NULL	NULL	-7.205313	112.736215
+20	0	1	Rumah HS. Cokroaminoto	Jalan Peneleh	NULL	NULL	-7.253422	112.74085
+21	0	1	Rumah WR. Supratman	Jalan Mangga	NULL	NULL	-7.250971	112.753621
+22	0	2	Gereja Graha Bethany	Jalan Nginden Intan Timur I/29	NULL	NULL	-7.307155	112.772361
+23	0	2	Gereja Kathedral	Jalan Polisi Istimewa	NULL	NULL	-7.280667	112.7426
+24	0	2	Gereja Katholik kelahiran santa perawan maria	Jalan Kepanjen 4-6 Surabaya	NULL	NULL	-7.242257	112.736805
+25	0	2	Klenteng Boen Bio	Jalan Kapasan	NULL	NULL	-7.239277	112.748414
+26	0	2	Klenteng Hok An Kiong (Klenteng Ma Co Po)	Jalan Slompretan	NULL	NULL	-7.239623	112.74099
+27	0	2	Klenteng Hong Tiek Hyan	Jalan Dukuh Gang II/2	NULL	NULL	-7.236329	112.743909
+28	0	2	Klenteng Sanggar Agung	?	NULL	NULL	-7.250133	112.803442
+29	0	2	Masjid Al Akbar	Jalan Masjid Al Akbar 1, Wisma Pagesangan	NULL	NULL	-7.336546	112.715096
+30	0	2	Masjid Muhammad Cheng Hoo	Jalan Masjid Al Akbar 1, Wisma Pagesangan	NULL	NULL	-7.252192	112.746956
+31	0	2	Masjid Sunan Ampel	Jalan KH Mas Mansyur	NULL	NULL	-7.235118	112.742876
+32	0	2	Patung Budha 4 Wajah	Jalan Sukolilo	NULL	NULL	-7.24959	112.80327
+33	0	2	Patung Joko Dolog	Jalan Taman Apsari	NULL	NULL	-7.264897	112.742809
+34	0	2	Pura Jagat Karana	Jalan Lumba-lumba	NULL	NULL	-7.229623	112.723898
+35	0	2	Rumah Abu	Jalan Karet 50 dan 72	NULL	NULL	-7.237952	112.73871
+36	0	3	Museum Emputantular	Buduran, Sidoarjo	NULL	NULL	-7.430592	112.721399
+37	0	3	Museum House Loka Jala Crana	Kobangdikal	NULL	NULL	-7.219714	112.716869
+38	0	3	Museum House of Sampoerna	Jalan Taman Sampoerna	NULL	NULL	-7.230964	112.733404
+39	0	3	Museum Kesehatan dr. Adhyatma, MPH	Jalan Indrapura 172	NULL	NULL	-7.240038	112.731495
+40	0	3	Museum Sepuluh Nopember	Jalan Pahlawan	NULL	NULL	-7.245908	112.73791
+41	0	4	Monumen Bahari	Persimpangan Jalan Darmo dan Jalan Dipenegoro	NULL	NULL	-7.294874	112.738865
+42	0	4	Monumen Bambu Runcing	Jalan Panglima Sudirman	NULL	NULL	-7.267763	112.744375
+43	0	4	Monumen Gubernur Soerjo	Jalan Gubernur Suryo	NULL	NULL	-7.263649	112.742798
+44	0	4	Monumen Jalas Veva Jayamahe	Kompleks Koarmatim	NULL	NULL	-7.195265	112.739547
+45	0	4	Monumen Jendral Sudirman	Jalan Yos Sudarso	NULL	NULL	-7.261531	112.746446
+46	0	4	Monumen Kapal Selam	Jl. Pemuda 39 Surabaya 60100	NULL	NULL	-7.265714	112.750301
+47	0	4	Monumen Mayangkara	Jalan A. Yani	NULL	NULL	-7.265799	112.750121
+48	0	4	Monumen Pangeran Dipenegoro	Jalan Dipenegoro Raya	NULL	NULL	-7.279432	112.730261
+49	0	4	Monumen Perjuangan Polri	Jalan Darmo	NULL	NULL	-7.280826	112.740904
+50	0	4	Monumen Ronggolawe	Jalan Gunungsari	NULL	NULL	-7.299535	112.731439
+51	0	4	Monumen Tugu Pahlawan	Jalan Pahlawan	NULL	NULL	-7.246046	112.737877
+52	0	4	Monumen Wira Surya	Jalan	NULL	NULL	-7.298726	112.73807
+53	0	5	Taman Apsari	Jalan Taman Apsari	NULL	NULL	-7.263649	112.742554
+54	0	5	Taman Buah	Jalan Undaan	NULL	NULL	-7.255329	112.743091
+55	0	5	Taman Bungkul	Jalan Raya Darmo	NULL	NULL	-7.291214	112.739804
+56	0	5	Taman Dolog (PELANGI)	Bundaran Dolog	NULL	NULL	-7.327735	112.73113
+57	0	5	Taman Ekspresi	Gelengkah	NULL	NULL	-7.257258	112.743678
+58	0	5	Taman Flores	Jalan Flores	NULL	NULL	-7.27832	112.748849
+59	0	5	Taman Kebun Bibit Wonoreio (FLORA)	Wonorejo	NULL	NULL	-7.295364	112.76176
+60	0	5	Taman Ketabang (PRESTASI)	Ketabang Kali	NULL	NULL	-7.261175	112.742761
+61	0	5	Taman Kunang-kunang	-	NULL	NULL	-7.319004	112.784104
+62	0	5	Taman Lansia	Jalan Raya Gubeng	NULL	NULL	-7.271014	112.75013
+63	0	5	Taman Mayangkara	Wonokromo	NULL	NULL	-7.30675	112.735486
+64	0	5	Taman Mundu	Jalan Tambaksan	NULL	NULL	-7.251538	112.754508
+65	0	5	Taman Nambangan	Nambangan	NULL	NULL	-7.222367	112.787076
+66	0	5	Taman Persahabatan	Jalan Dr. Soetomo	NULL	NULL	-7.283295	112.736033
+67	0	5	Taman Ronggolawe	Jalan Joyoboyo	NULL	NULL	-7.299642	112.731248
+68	0	5	Taman Sulawesi	Jalan Sulawesi	NULL	NULL	-7.276718	112.745903
+69	0	5	Taman Teratai	Jalan Teratai	NULL	NULL	-7.254989	112.75544
+70	0	6	BG Junction	Jalan Bubutan 1-7 Surabaya	NULL	NULL	-7.255497	112.733887
+71	0	6	Ciputra World	Jalan Mayjen Sungkono	NULL	NULL	-7.299833	112.726826
+72	0	6	City of Tomorrow (CITO)	Jalan Bundaran Waru	NULL	NULL	-7.299833	112.726826
+73	0	6	Darmo Trade Center (DTC)	Jalan Raya Wonokromo	NULL	NULL	-7.302345	112.737685
+74	0	6	East Cost Center	Perumahan Pakuwo City, Kejawan Keputih	NULL	NULL	-7.276532	112.804773
+75	0	6	Empire Palace	Jalan Blauran 57-75	NULL	NULL	-7.257455	112.734327
+76	0	6	Galaxy Mall	Jalan Kertajaya	NULL	NULL	-7.276165	112.781886
+77	0	6	Golden City	Jalan Abdul Wahab Slamen	NULL	NULL	-7.288462	112.706658
+78	0	6	Grand City	Jalan Kusuma Bangsa,  Jalan Walikota Mustajab	NULL	NULL	-7.26176	112.750711
+79	0	6	Hi- Tech Mall / THR Mall	Jalan Kusuma Bangsa	NULL	NULL	-7.252432	112.750527
+80	0	6	ITC Surabaya	Jalan Gembong 20-30	NULL	NULL	-7.24082	112.746021
+81	0	6	Jembatan Merah Plaza (JMP)	Jalan Rajawali	NULL	NULL	-7.23602	112.737814
+82	0	6	Maspion Square	Jalan A. Yani	NULL	NULL	-7.313423	112.734999
+83	0	6	Pakuwon Trade Center (PTC)	Jalan Bukit Darmo Boulevard	NULL	NULL	-7.290692	112.672117
+84	0	6	Pasar Atom & Pasar Atom Mall	Jalan Bunguran 45	NULL	NULL	-7.240692	112.744905
+85	0	6	Plaza Marina	Jalan Margorejo Indah	NULL	NULL	-7.313795	112.737192
+86	0	6	Pusat Grosir Surabaya (PGS)	Jalan Dupak 1	NULL	NULL	-7.246328	112.733271
+87	0	6	Royal Plaza	Jalan A. Yani	NULL	NULL	-7.308751	112.735159
+88	0	6	Surabaya Plaza / Delta Plaza	Jalan Pemuda	NULL	NULL	-7.264958	112.748667
+89	0	6	Surabaya Town Square (Sutos)	Jalan Adityawarman	NULL	NULL	-7.294565	112.731426
+90	0	6	Tunjungan Electronic Center (TEC)	Jalan Tunjungan	NULL	NULL	-7.256785	112.737814
+91	0	6	Tunjungan Plaza (TP)	Jalan Basuki Rachamat 16-18	NULL	NULL	-7.262625	112.740596
+92	0	6	World Trade Center (WTC)	Jalan Pemuda Surabaya	NULL	NULL	-7.263415	112.74768
+93	0	7	Kenjeran Ria (Ken Park)	Jalan Sukolilo	NULL	NULL	-7.250303	112.80083
+94	0	7	Pantai Kenjeran	Jalan Sukolilo	NULL	NULL	-7.238106	112.795705
+95	0	7	Eco Wisata Mangrove Wonorejo	Jalan Kelurahan Wonorejo	NULL	NULL	-7.310028	112.842794
+96	0	7	Kebun Binatang	Jalan Setail Surabaya	NULL	NULL	-7.296098	112.736537
+97	0	8	Pasar Genteng	Jalan Genteng Besar	NULL	NULL	-7.258466	112.739282
+98	0	8	Pasar Bunga Kayoon	Jalan Kayoon	NULL	NULL	-7.268758	112.748141
+99	0	8	Pasar Bunga Kebun Bibit Bratang	Jalan Bratang Binangun	NULL	NULL	-7.297135	112.760485
+100	0	8	Pasar Bunga Angrek Darmo	Jalan Darmo Satelit	NULL	NULL	-7.290756	112.700119
+101	0	8	Pasar Buah Peneleh Baru	Jalan Tanjungsari 96 	NULL	NULL	-7.256881	112.699553
+102	0	8	Pasar Ikan Hias Gunung Sari	Jalan Gunungsari	NULL	NULL	-7.306186	112.723846
+103	0	8	Fresh & Green Market Citra Land	Jalan Taman Puspa Raya Citra Land	NULL	NULL	-	-
+104	0	9	Eat and Eat	-	NULL	NULL	-7.26996	112.741311
+105	0	9	Food Festival	-	NULL	NULL	-7.276516	112.804712
+106	0	9	G-WALK	-	NULL	NULL	-7.290628	112.672146
+107	0	9	Kawasan Kedungdoro	-	NULL	NULL	-7.264735	112.730688
+108	0	9	Kawasan Manyar	-	NULL	NULL	-7.279815	112.7675
+109	0	9	Kawasan Ria	-	NULL	NULL	-7.250638	112.802627
+110	0	9	Kawasan SCTV	-	NULL	NULL	-7.281316	112.698244
+111	0	9	Kawasan Tidar	-	NULL	NULL	-7.281316	112.698244
+112	0	9	The Loop	-	NULL	NULL	-7.298833	112.694898
+113	0	10	Pakuwon Golf	Vila Bukit Regensi Pakuwon Indah - Surabaya	NULL	NULL	-7.295087	112.663461
+114	0	10	Yani Golf	Jl. Hayam Wuruk 54 A, Surabaya	NULL	NULL	-7.301557	112.715775
+115	0	10	Brawijaya Golf Driving Range	-	NULL	NULL	-7.301642	112.724873
+116	0	10	Bukit Darmo Golf	Jl. Bukit Darmo Golf Blok G-2, Surabaya	NULL	NULL	-7.288957	112.687748
+117	0	10	Ciputra Golf	Jl. Citraland Utama, Citraland Surabaya	NULL	NULL	-7.292214	112.637692
+118	0	10	Graha Family	Jl. Raya Golf Graha Family Surabaya	NULL	NULL	-7.294661	112.689811
+NULL		NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL
