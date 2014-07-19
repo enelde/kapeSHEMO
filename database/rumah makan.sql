13121c809ab8a50c60cda5ea70f1458ff2821379
enelde <sushierz.sashimierz@gmail.com>
2014-07-19 22:48:08 +0700

    no message

---
+++ database/rumah makan.sql
@@ -0,0 +1,10 @@
+1	1	Ayam Primarasa	Jalan Kusuma Bangsa	(031) 5313000	-7.260052	112.751064
+2	1	Ayam Mbok Berek	Jalan Jemursari III/Kav 5 no.1	(031) 8431286	-7.322473	112.743522
+3	1	Bebek D'Kwek	Jalan Manyar Kertoarjo V/4	(031) 70999965	-7.279507	112.765204
+4	2	LB Rajawali	Jalan Rajawali (depan SPBU)	NULL	-7.234756	112.732472
+5	2	LB Pak Gendut	Jalan Kranggan	NULL	-7.255539	112.731373
+6	3	Soto Pak Sadi	JalanAmbengan 3A	(031) 5323998	-7.253996	112.744547
+7	3	Soto Madura	Jalan Raya Dukuh Kupang Barat	NULL	-7.282912	112.708116
+8	4	Depot Wijaya	Jalan Demak 311	NULL	-7.238809	112.720796
+9	4	Pak Dono	Jalan Kalasan 28	NULL	-7.258057	112.757432
+10	5	Depot Sari Darmo	Mayjend Sungkono	NULL	-7.288297	112.708158
